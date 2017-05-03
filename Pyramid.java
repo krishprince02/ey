29673ea219bc8e6258da675e1714c7b550613dcb
krishprince02 <krishnareddy.av04@gmail.com>
2017-05-04 00:21:00 +0530

    i commited Pyramid

--- Pyramid.java
+++ Pyramid.java
@@ -1,4 +1,4 @@
-public static void drawPyramidPattern() {
+kdjskjdcs:wqublic static void drawPyramidPattern() {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5 - i; j++) {
                 System.out.print(" ");
